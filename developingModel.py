b132487e68a2410369216b1349d487c0e5d31c68
Srivathsan Subramanian <ss4@illinois.edu>
2018-01-03 00:37:57 -0800

    developing model script

---
+++ developingModel.py
@@ -0,0 +1,50 @@
+from pickle import load
+
+def load_doc(filename):
+    file = open(filename, 'r')
+    text = file.read()
+    file.close()
+    return text
+
+def load_set(filename):
+    doc = load_doc(filename)
+    dataset = list()
+
+    for line in doc.split('\n'):
+        if len(line) < 1:
+            continue
+        identifier = line.split('.')[0]
+        dataset.append(identifier)
+    return set(dataset)
+
+def load_clean_descriptions(filename, dataset):
+    doc = load_doc(filename)
+    descriptions = dict()
+    for line in doc.split('\n'):
+        tokens = line.split()
+
+        image_id, image_desc = tokens[0],tokens[1]
+
+        if image_id in dataset:
+            if image_id not in descriptions:
+                descriptions[image_id] = list()
+
+            desc = 'startseq' + ' '.join(image_desc) + 'endseq'
+            descriptions[image_id].append(desc)
+    return descriptions
+
+def load_photo_features(filename, dataset):
+    all_features = load(open(filename, 'rb'))
+    features = {k: all_features[k] for k in dataset}
+    return features
+
+filename = '/Users/Sri/Desktop/Projects/TensorFlow/AutoCaption/Flickr8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+
+print('Dataset: %d' %len(train))
+
+train_descriptions = load_clean_descriptions('/Users/Sri/Desktop/Projects/TensorFlow/AutoCaption/descriptions.txt',train)
+print('Descriptions: train = %d'%len(train_descriptions))
+
+train_features = load_photo_features('features.pkl',train)
+print('Photos: train=%d' % len(train_features))
