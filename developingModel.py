130338166d3ef8d6252d553e9bab61b5b15636bd
Srivathsan Subramanian <ss4@illinois.edu>
2018-01-03 13:53:18 -0800

    developing model

--- developingModel.py
+++ developingModel.py
@@ -1,4 +1,17 @@
 from pickle import load
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.utils import plot_model
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout
+from keras.layers.merge import add
+from keras.callbacks import ModelCheckpoint
 
 def load_doc(filename):
     file = open(filename, 'r')
@@ -38,7 +51,61 @@ def load_photo_features(filename, dataset):
     features = {k: all_features[k] for k in dataset}
     return features
 
+def to_lines(descriptions):
+    all_desc = list()
+    for key in descriptions.keys():
+        [all_desc.append(d) for d in descriptions[key]]
+    return all_desc
+
+def create_tokenizer(descriptions):
+    lines = to_lines(descriptions)
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+def max_length(descriptions):
+    lines = to_lines(descriptions)
+    return max(len(d.split()) for d in lines)
+
+def create_sequences(tokenizer, max_length, descriptions, photos):
+    X1, X2, y = list(), list(), list()
+    for key, desc_list in descriptions.items():
+        for desc in desc_list:
+            seq = tokenizer.texts_to_sequences([desc])[0]
+            for i in range(1, len(seq))
+                in_seq, out_seq = seq[:i],seq[i]
+                in_seq = pad_sequences([in_seq], maxlen = max_length)[0]
+                out_seq = to_categorical([out_seq],num_classes = vocab_size)[0]
+
+                X1.append(photos[key][0])
+                X2.append(in_seq)
+                y.append(out_seq)
+    return array(X1), array(X2), array(y)
+
+def define_model(vocald_size, max_length):
+    inputs1 = Input(shape=(4096,))
+    fe1 = Dropout(0.1)(inputs1)
+    fe2 = Dense(256, activation='relu')(fe1)
+
+    inputs2 = Input(shape=(max_length,))
+    se1 = Embedding(vocab_size, 256, max_zero = True)(inputs2)
+    se2 = Droupout(0.5)(se1)
+    se3 = LSTM(256)(se2)
+
+    decoder1 = add([fe2,se3])
+    decoder2 = Dense(256, activation = 'relu')(decoder1)
+    outputs = Dense(vocab_size, activation = 'softmax')(decoder2)
+
+    model = Model(inputs = [inputs1, inputs2], outputs = outputs)
+    model.compile(loss = 'categorical_crossentropy',optimizer = 'adam')
+
+    print(model.summary())
+    plot_model(model, to_file = 'model.png', show_shapes = True)
+    return model
+
 filename = '/Users/Sri/Desktop/Projects/TensorFlow/AutoCaption/Flickr8k_text/Flickr_8k.trainImages.txt'
+
+#training
 train = load_set(filename)
 
 print('Dataset: %d' %len(train))
@@ -48,3 +115,32 @@ print('Descriptions: train = %d'%len(train_descriptions))
 
 train_features = load_photo_features('features.pkl',train)
 print('Photos: train=%d' % len(train_features))
+
+tokenizer = create_tokenizer(train_descriptions)
+vocald_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d'%max_length)
+
+X1train, X2train, ytrain = create_sequences(tokenizer, max_length, train_descriptions)
+
+
+
+#testing
+filename = '/Users/Sri/Desktop/Projects/TensorFlow/AutoCaption/Flickr8k_text/Flickr_8k.devImages.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+
+test_descriptions = load_clean_descriptions('descriptions.txt',test)
+print('Descriptions: test=%d'%len(test_descriptions))
+
+test_features = load_photo_features('features.pkl',test)
+print('Photos: test=%d'%len(test_features))
+
+X1test, X2test, ytest = create_sequences(tokenizer, max_length, test_descriptions)
+
+#fit model
+model = define_model(vocab_size, max_length)
+
+filepath = 'model-ep{epoch:03d}-loss{loss.3f}-val_loss{val_loss:.3f}.h5'
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss',verbose = 1, save_best_only = True, mode = 'min')
+
+model.fit([X1train,X2train],ytrain, epochs=20, verbose=2,callbacks=[checkpoint],validation_data=([X1test,X2test],ytest))
