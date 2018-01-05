b36092068345fe3e36d63a6eb85a9235fdf7e348
Srivathsan Subramanian <ss4@illinois.edu>
2018-01-05 11:29:41 -0800

    changed prepText and fixed descriptions

--- prepText.py
+++ prepText.py
@@ -1,67 +1,62 @@
 import string
 
+# load doc into memory
 def load_doc(filename):
-    #open as read only
-    file = open(filename,'r')
+	file = open(filename, 'r')
+	text = file.read()
+	file.close()
+	return text
 
-    text = file.read()
-
-    file.close()
-    return text
-
+# extract descriptions for images
 def load_descriptions(doc):
-    mapping = dict()
-
-    for line in doc.split('\n'):
-        tokens = line.split()
-        if len(line) < 2:
-            continue
-
-        image_id, image_desc = tokens[0], tokens[1:]
-        image_id = image_id.split('.')[0]
-
-        image_desc = ' '.join(image_desc)
-
-        if image_id not in mapping:
-            mapping[image_id] = list()
-        mapping[image_id].append(image_desc)
-    return mapping
+	mapping = dict()
+	for line in doc.split('\n'):
+		tokens = line.split()
+		if len(line) < 2:
+			continue
+		image_id, image_desc = tokens[0], tokens[1:]
+		image_id = image_id.split('.')[0]
+		image_desc = ' '.join(image_desc)
+		if image_id not in mapping:
+			mapping[image_id] = list()
+		mapping[image_id].append(image_desc)
+	return mapping
 
 def clean_descriptions(descriptions):
-    table = str.maketrans('','',string.punctuation)
-    for key, desc_list in descriptions.items():
-        for i in range(len(desc_list)):
-            desc = desc_list[i]
-            desc = desc.split()
-            desc = [word.lower() for word in desc]
-            desc = [w.translate(table) for w in desc]
-            desc = [word for word in desc if word.isalpha()]
-            desc_list[i] = ' '.join(desc)
+	table = str.maketrans('', '', string.punctuation)
+	for key, desc_list in descriptions.items():
+		for i in range(len(desc_list)):
+			desc = desc_list[i]
+			desc = desc.split()
+			desc = [word.lower() for word in desc]
+			desc = [w.translate(table) for w in desc]
+			desc = [word for word in desc if len(word)>1]
+			desc = [word for word in desc if word.isalpha()]
+			desc_list[i] =  ' '.join(desc)
 
+# convert the loaded descriptions into a vocabulary of words
 def to_vocabulary(descriptions):
-    all_desc = set()
-    for key in descriptions.keys():
-        [all_desc.update(d.split()) for d in descriptions[key]]
-    return all_desc
+	all_desc = set()
+	for key in descriptions.keys():
+		[all_desc.update(d.split()) for d in descriptions[key]]
+	return all_desc
 
+# save descriptions to file, one per line
 def save_descriptions(descriptions, filename):
-    lines = list()
-    for key, desc_list in descriptions.items():
-        for desc in desc_list:
-            lines.append(key + ' '+ desc)
-    data = '\n'.join(lines)
-    file = open(filename,'w')
-    file.write(data)
-    file.close()
+	lines = list()
+	for key, desc_list in descriptions.items():
+		for desc in desc_list:
+			lines.append(key + ' ' + desc)
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
 
-filename = '/Users/Sri/Desktop/Projects/TensorFlow/AutoCaption/Flickr8k_text/Flickr8k.token.txt'
+filename = 'Flickr8k_text/Flickr8k.token.txt'
 doc = load_doc(filename)
-
 descriptions = load_descriptions(doc)
-print('Loaded: %d'%len(descriptions))
-
+print('Loaded: %d ' % len(descriptions))
 clean_descriptions(descriptions)
 vocabulary = to_vocabulary(descriptions)
-print('Vocabulary Size: %d'%len(vocabulary))
-
+print('Vocabulary Size: %d' % len(vocabulary))
 save_descriptions(descriptions, 'descriptions.txt')
